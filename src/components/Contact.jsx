3c7961ca6863fe0943bffe7e4e5bc505e768312d
engrkashifkhan <kashifkhn6464ak@gmail.com>
2025-01-22 23:31:39 +0500

    changing icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,29 +1,31 @@
-import { RiMailLine, RiMapLine, RiPhoneLine } from '@remixicon/react'
+import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
 import React from 'react'
 
 function Contact() {
   return (
-    <section className='max-w-7xl mx-auto mt-20' id='contact'>
+    <section className='max-w-7xl mx-auto mt-20' id='Contact'>
         <div className='my-20'>
             <h2 className='text-xl lg:text-3xl tracking-tight text-center uppercase mb-12'>Contact Us</h2>
             <p className='max-w-2xl text-lg mb-12 text-center mx-auto'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam eligendi, quod vitae soluta iure aspernatur.</p>
            <div className='flex flex-col lg:flex-row justify-around mx-4'>
             <div className='flex mb-8 lg:mb-0'>
-                <RiPhoneLine className='text-3xl mr-4 '/>
+                <FaWhatsapp className='text-3xl mr-4 '/>
                 <div>
                     <p className='text-lg font-semibold'>WhatsaApp</p>
                     <p className='text-neutral-600'>03339447275</p>
                 </div>
             </div>
             <div className='flex mb-8 lg:mb-0'>
-                <RiMailLine className='text-3xl mr-4 '/>
+                <MdOutlineEmail className='text-3xl mr-4 '/>
                 <div>
                     <p className='text-lg font-semibold'>Email</p>
                     <p className='text-neutral-600'>kashifkhn6464ak@gmail.com</p>
                 </div>
             </div>
             <div className='flex mb-8 lg:mb-0'>
-                <RiMapLine className='text-3xl mr-4 '/>
+                <FaLocationDot className='text-3xl mr-4 '/>
                 <div>
                     <p className='text-lg font-semibold'>Address</p>
                     <p className='text-neutral-600'>Tehkal peshawar, pakistan</p>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { RiFacebookFill, RiInstagramFill, RiTwitterFill } from '@remixicon/react'
+import { FaInstagramSquare } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 import React from 'react'
 
 function Footer() {
@@ -6,22 +7,16 @@ function Footer() {
     <section className='max-w-7xl mx-auto'>
         <div className='flex flex-col md:flex-row justify-between items-center border-t-2 py-4'>
             <div className='flex space-x-4 mb-2'>
-                <a href="https://www.facebook.com"
-                target='_blank'
-                reel="noopener noreferrer"
-                aria-label='visit our pacebook page'
-                ><RiFacebookFill/>
-                </a>
-                <a href="https://www.twitter.com"
+                <a href="https://kashifkhn6464ak@gmail.com"
                 target='_blank'
                 reel="noopener noreferrer"
                 aria-label='visit our twitter page'
-                ><RiTwitterFill/>
-                </a> <a href="https://www.instagram.com"
+                ><MdOutlineEmail />
+                </a> <a href="https://instagram.com/kashifkhan4676"
                 target='_blank'
                 reel="noopener noreferrer"
                 aria-label='visit our instagram page'
-                ><RiInstagramFill/>
+                ><FaInstagramSquare />
                 </a>
             </div>
             <p className='text-sm'> &Copy, 2025 Kashif. All rights reserved</p>
